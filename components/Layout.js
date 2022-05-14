8bd06bd2a940e5432ac3175c9c819609986c61d9
golam@@0177 <rabbymohiuddin@gmail.com>
2022-05-14 12:26:29 +0600

    added messenger

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-
+import MessengerCustomerChat from "react-messenger-customer-chat";
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
+      <MessengerCustomerChat
+        pageId="102656942454838"
+        appId="186297763311492"
+        // htmlRef="<REF_STRING>"
+      />
       {children}
       <Footer />
     </>
