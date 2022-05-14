280c4f1471d971e53901bbee3e081c0fb20b0f08
golam@@0177 <rabbymohiuddin@gmail.com>
2022-05-14 14:29:26 +0600

    creditials changed

--- components/Layout.js
+++ components/Layout.js
@@ -3,12 +3,21 @@ import Footer from "./Footer";
 import Header from "./Header";
 import MessengerCustomerChat from "react-messenger-customer-chat";
 const Layout = ({ children }) => {
+  // 285211272432211 - page code - trust holidays
+  //developer code - 5157237834392316 - from reza sir's fb account
+  // testing purposes site - https://melodious-monstera-afe190.netlify.app/
+  /**
+   * pageId="102656942454838"
+     appId="186297763311492"
+     for testing purpose page
+
+   */
   return (
     <>
       <Header />
       <MessengerCustomerChat
-        pageId="102656942454838"
-        appId="186297763311492"
+        pageId="285211272432211"
+        appId="5157237834392316"
         // htmlRef="<REF_STRING>"
       />
       {children}
